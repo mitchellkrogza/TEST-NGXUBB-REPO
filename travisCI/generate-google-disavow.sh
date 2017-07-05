d9e263973f6632afb10acc26db9108a1d9eaad12
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-05 15:28:19 +0200

    Script change to generate google disavow and general cleaning up of scripts.

--- travisCI/generate-google-disavow.sh
+++ travisCI/generate-google-disavow.sh
@@ -37,10 +37,16 @@ _input1=$TRAVIS_BUILD_DIR/_generator_lists/bad-referrers.list
 # Create Google Disavow File
 # **************************
 
+#sudo truncate -s 0 $TRAVIS_BUILD_DIR/google-disavow.txt
+#for line in $(cat $_input1); do
+#printf "domain:${line}\n" >> $TRAVIS_BUILD_DIR/google-disavow.txt
+#done
+
 sudo truncate -s 0 $TRAVIS_BUILD_DIR/google-disavow.txt
-for line in $(cat $_input1); do
-printf "domain:${line}\n" >> $TRAVIS_BUILD_DIR/google-disavow.txt
-done
+while IFS= read -r LINE
+do
+printf '%s%s\n' "domain:" "${LINE}" >> $TRAVIS_BUILD_DIR/google-disavow.txt
+done < $_input1
 
 exit 0
 
