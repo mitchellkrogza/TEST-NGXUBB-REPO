5d4a700c084abd8b1b05b4736b89ffaace4d000f
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-06 12:12:48 +0200

    curl test 32

--- travisCI/run-curl-tests.sh
+++ travisCI/run-curl-tests.sh
@@ -3,8 +3,8 @@
 
 #STATUSCODE1=$(curl -A "80legs" http://localhost:9000/index.php &> /dev/stderr --write-out "%{http_code}") | if test $STATUSCODE1 52; then printf '%s\n\n' "BAD BOT TEST PASSED"; exit 0; else printf '%s\n\n' "BAD BOT TEST FAILED"; exit 1; fi
 
-truncate -s 0 $TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest1.txt
-curl -A "80legs" http://localhost:9000/index.php >> $TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest1.txt
+#truncate -s 0 $TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest1.txt
+curl -A "80legs" http://localhost:9000/index.php &>> $TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest1.txt
 cat $TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest1.txt
 grep -i '(52)' $TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest1.txt
 
