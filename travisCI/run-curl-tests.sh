78184ca68ae2a90cf2a5a6c124f1d747dac55842
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-07 13:06:23 +0200

    switch vhost file and curl_tests to port 80

--- travisCI/run-curl-tests.sh
+++ travisCI/run-curl-tests.sh
@@ -14,7 +14,7 @@ _curltest6=$TRAVIS_BUILD_DIR/travisCI/_curl_tests/curltest6.txt
 # Function Curl Test 1 - Check for Bad Bot "80legs"
 run_curltest1 () {
 truncate -s 0 $_curltest1
-curl -A "80legs" http://localhost:9000/index.php 2> $_curltest1
+curl -A "80legs" http://localhost:80/index.php 2> $_curltest1
 if grep -i '(52)' $_curltest1; then
    echo 'BAD BOT DETECTED - TEST PASSED'
 else
@@ -26,7 +26,7 @@ fi
 # Function Curl Test 2 - Check for Bad Bot "masscan"
 run_curltest2 () {
 truncate -s 0 $_curltest2
-curl -A "masscan" http://localhost:9000/index.php 2> $_curltest2
+curl -A "masscan" http://localhost:80/index.php 2> $_curltest2
 if grep -i '(52)' $_curltest2; then
    echo 'BAD BOT DETECTED - TEST PASSED'
 else
@@ -38,7 +38,7 @@ fi
 # Function Curl Test 3 - Check for Bad Referrer "100dollars-seo.com"
 run_curltest3 () {
 truncate -s 0 $_curltest3
-curl -I http://localhost:9000/index.php -e http://100dollars-seo.com 2> $_curltest3
+curl -I http://localhost:80/index.php -e http://100dollars-seo.com 2> $_curltest3
 if grep -i '(52)' $_curltest3; then
    echo 'BAD REFERRER DETECTED - TEST PASSED'
 else
@@ -50,7 +50,7 @@ fi
 # Function Curl Test 4 - Check for Bad Referrer "zx6.ru"
 run_curltest4 () {
 truncate -s 0 $_curltest4
-curl -I http://localhost:9000/index.php -e http://zx6.ru 2> $_curltest4
+curl -I http://localhost:80/index.php -e http://zx6.ru 2> $_curltest4
 if grep -i '(52)' $_curltest4; then
    echo 'BAD REFERRER DETECTED - TEST PASSED'
 else
@@ -62,7 +62,7 @@ fi
 # Function Curl Test 5 - Check for Good Bot "GoogleBot"
 run_curltest5 () {
 truncate -s 0 $_curltest5
-curl -v -A "GoogleBot" http://localhost:9000/index.php 2>&1 >> $_curltest5
+curl -v -A "GoogleBot" http://localhost:80/index.php 2>&1 >> $_curltest5
 if grep -i 'Welcome' $_curltest5; then
    echo 'GOOD BOT ALLOWED THROUGH - TEST PASSED'
 else
@@ -74,7 +74,7 @@ fi
 # Function Curl Test 6 - Check for Good Bot "BingBot"
 run_curltest6 () {
 truncate -s 0 $_curltest6
-curl -v -A "BingBot" http://localhost:9000/index.php 2>&1 >> $_curltest6
+curl -v -A "BingBot" http://localhost:80/index.php 2>&1 >> $_curltest6
 if grep -i 'Welcome' $_curltest6; then
    echo 'GOOD BOT ALLOWED THROUGH - TEST PASSED'
 else
