0cfdb599e21d7510c57bab9cd686c7490fccb9fe
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-07 16:10:14 +0200

    write local ip into vhost file

--- travisCI/get-local-ip.sh
+++ travisCI/get-local-ip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 _myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-sed -i -e 's/*:80/$_myip:80/g' $TRAVIS_BUILD_DIR/travisCI/default.vhost
+_file=$TRAVIS_BUILD_DIR/travisCI/default.vhost
+sed -i -e "s/*:80/$_myip:80/g" "$_file"
 exit 0
